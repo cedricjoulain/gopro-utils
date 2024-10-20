c5858833485778f559372b43542fd6b7ab17d2c9
Cedric Joulain <cedric.joulain@gmail.com>
2024-10-20 14:24:36 +0200

    change: no need to store if 0

--- telemetry/gps5.go
+++ telemetry/gps5.go
@@ -7,12 +7,12 @@ import (
 
 // GPS sentence with lat/lon/alt/speed/3d speed
 type GPS5 struct {
-	Latitude  float64 `json:"lat"`    // degrees lat
-	Longitude float64 `json:"lon"`    // degrees lon
-	Altitude  float64 `json:"alt"`    // meters above wgs84 ellipsoid ?
-	Speed     float64 `json:"spd"`    // m/s
-	Speed3D   float64 `json:"spd_3d"` // m/s, standard error?
-	TS        int64   `json:"utc"`
+	Latitude  float64 `json:"lat"`              // degrees lat
+	Longitude float64 `json:"lon"`              // degrees lon
+	Altitude  float64 `json:"alt,omitempty"`    // meters above wgs84 ellipsoid ?
+	Speed     float64 `json:"spd,omitempty"`    // m/s
+	Speed3D   float64 `json:"spd_3d,omitempty"` // m/s, standard error?
+	TS        int64   `json:"utc,omitempty"`
 }
 
 func (gps *GPS5) Parse(bytes []byte, scale *SCAL) error {
